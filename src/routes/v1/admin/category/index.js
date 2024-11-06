f5292df654b82d505693ea9d49623424a983d0bb
vivekf60 <vivek@pentagoat.com>
2024-11-06 18:55:39 +0530

    worked on category get all apis

--- src/routes/v1/admin/category/index.js
+++ src/routes/v1/admin/category/index.js
@@ -1,65 +1,65 @@
 const { Router } = require('express');
 const {
-	isAuthenticated,
+  isAuthenticated,
 } = require('../../../../../brain/middleware/isAuthenticated');
 const AccessControlMiddleware = require('../../../../../brain/middleware/accessControl.middleware');
 const {
-	RBAC_ACTIONS,
-	RBAC_RESOURCES,
+  RBAC_ACTIONS,
+  RBAC_RESOURCES,
 } = require('../../../../../brain/utils/enums');
 const {
-	addCategory,
-	getAllCategories,
-	updateCategory,
-	deleteCategoryImage,
+  addCategory,
+  getAllCategories,
+  updateCategory,
+  deleteCategoryImage,
 } = require('../../../../controller/v1/Admin/Category/category.controller');
 
 const categoryRouter = Router();
 
 categoryRouter.post(
-	'/add',
-	[
-		isAuthenticated,
-		AccessControlMiddleware.checkAccess(
-			RBAC_ACTIONS.CREATE_OWN,
-			RBAC_RESOURCES.CATEGORY
-		),
-	],
-	addCategory
+  '/add',
+  [
+    isAuthenticated,
+    AccessControlMiddleware.checkAccess(
+      RBAC_ACTIONS.CREATE_OWN,
+      RBAC_RESOURCES.CATEGORY
+    ),
+  ],
+  addCategory
 );
-categoryRouter.get(
-	'/all',
-	[
-		isAuthenticated,
-		AccessControlMiddleware.checkAccess(
-			RBAC_ACTIONS.READ_ANY,
-			RBAC_RESOURCES.CATEGORY
-		),
-	],
-	getAllCategories
+categoryRouter.post(
+  '/all',
+  [
+    isAuthenticated,
+    AccessControlMiddleware.checkAccess(
+      RBAC_ACTIONS.READ_ANY,
+      RBAC_RESOURCES.CATEGORY
+    ),
+  ],
+  getAllCategories
 );
 
 categoryRouter.put(
-	'/update/:categoryId',
-	[
-		isAuthenticated,
-		AccessControlMiddleware.checkAccess(
-			RBAC_ACTIONS.UPDATE_OWN,
-			RBAC_RESOURCES.CATEGORY
-		),
-	],
-	updateCategory
+  '/update/:categoryId',
+  [
+    isAuthenticated,
+    AccessControlMiddleware.checkAccess(
+      RBAC_ACTIONS.UPDATE_OWN,
+      RBAC_RESOURCES.CATEGORY
+    ),
+  ],
+  updateCategory
 );
 
 categoryRouter.delete(
-	'/image/:id/:uuid',
-	[
-		isAuthenticated,
-		AccessControlMiddleware.checkAccess(
-			RBAC_ACTIONS.UPDATE_OWN,
-			RBAC_RESOURCES.CATEGORY
-		),
-	],
-	deleteCategoryImage
+  '/image/:id/:uuid',
+  [
+    isAuthenticated,
+    AccessControlMiddleware.checkAccess(
+      RBAC_ACTIONS.UPDATE_OWN,
+      RBAC_RESOURCES.CATEGORY
+    ),
+  ],
+  deleteCategoryImage
 );
 module.exports = categoryRouter;
