37a7e65b8a29f021ff52ed8f3cfb3c326e813dcb
vivekf60 <vivek@pentagoat.com>
2024-11-28 14:49:23 +0530

    shifted to http2

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 const colors = require('colors');
-const mongoose = require('mongoose');
 const logger = require('./brain/utils/winston');
-const app = require('./index'); // get all the modele form the indexjs from modele.
+const app = require('./index');
 const { PORT } = require('./brain/utils/config');
-const port = PORT || 5050;
 const connectDB = require('./brain/utils/dbConnect');
+const http2 = require('http2');
+const fs = require('fs');
+const path = require('path');
 
-// Connect to the database
+const options = {
+  key: fs.readFileSync(path.join(__dirname, 'certificates', 'private-key.pem')),
+  cert: fs.readFileSync(
+    path.join(__dirname, 'certificates', 'public-cert.pem')
+  ),
+  allowHTTP1: true, // Enable fallback for HTTP/1.1
+};
+
+const port = PORT || 5050;
+
 connectDB()
-	.then(() => {
-		logger.info('Database connected successfully');
-		app.listen(port, () =>
-			logger.info(colors.bgGreen('Server Running On' + ' ' + `${PORT}`))
-		);
-	})
-	.catch((error) => {
-		logger.error('Failed to connect to the database', error);
-	});
+  .then(() => {
+    logger.info('Database connected successfully');
+
+    const server = http2.createSecureServer(options, app);
+
+    server.listen(port, () => {
+      logger.info(colors.bgGreen('HTTP/2 Server Running On' + ' ' + `${port}`));
+    });
+  })
+  .catch((error) => {
+    logger.error('Failed to connect to the database', error);
+  });
