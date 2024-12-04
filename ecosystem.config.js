6d7b3d26e85284571b50e75dddadd58d77af6c8c
vivekf60 <vivek@pentagoat.com>
2024-12-04 11:45:11 +0530

    Added pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: 'server.js',
       instances: 'max',
       exec_mode: 'cluster',
-      watch: true,
+      watch: false,
       max_memory_restart: '1024M',
       env: {
         NODE_ENV: 'development',
