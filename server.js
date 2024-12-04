8ae9eaf2886f3d5c6a88151f7c793aa9189e9e99
vivekf60 <vivek@pentagoat.com>
2024-12-04 13:08:46 +0530

    added vercel

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const colors = require('colors');
 const mongoose = require('mongoose');
 const logger = require('./brain/utils/winston');
-const app = require('./index'); // get all the modele form the indexjs from modele.
+const app = require('./index'); // Import the Express app from index.js
 const { PORT } = require('./brain/utils/config');
-const port = PORT || 5050;
 const connectDB = require('./brain/utils/dbConnect');
 
-// Connect to the database
+const port = PORT || 5050;
+
+// Connect to the database and start the server
 connectDB()
-	.then(() => {
-		logger.info('Database connected successfully');
-		app.listen(port, () =>
-			logger.info(colors.bgGreen('Server Running On' + ' ' + `${PORT}`))
-		);
-	})
-	.catch((error) => {
-		logger.error('Failed to connect to the database', error);
-	});
+  .then(() => {
+    logger.info('Database connected successfully');
+    app.listen(port, () => {
+      logger.info(colors.bgGreen(`Server Running On ${port}`));
+    });
+  })
+  .catch((error) => {
+    logger.error('Failed to connect to the database', error);
+  });
+
+module.exports = app; // Exporting app for Vercel to use
